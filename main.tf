63f890c965b37848361ff0deacba4286178ae449
lucasdlima11 <lucaasliima111@gmail.com>
2024-07-13 11:54:12 -0300

    add tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform Example"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "ami_id" {
+  description = "The AMI to use for the instance"
+  default     = "ami-0c55b159cbfafe1f0"  # Exemplo de AMI, substitua conforme necessário
+}
